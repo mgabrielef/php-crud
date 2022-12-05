fe9508f1491b0dc950d01f427b2d1a7d72593f00
Gabriele Ferreira <mariagabrieleoliveira@gmail.com>
2022-12-05 19:29:28 -0300

    first try

---
+++ alterar.php
@@ -0,0 +1,42 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<?php
+$pdo = new PDO('mysql:host=localhost;dbname=cadastro_cliente', 'root', '');
+
+if (isset($_GET['cod_cliente'])) {
+    $cod_cliente = (int)$_GET['cod_cliente'];
+    $sql = $pdo->prepare("SELECT * FROM tab_clientes WHERE cod_cliente = $cod_cliente");
+    $sql->execute();
+    $clientes = $sql->fetchAll();
+
+    foreach ($clientes as $cliente) {
+        echo "<form method='POST'>";
+        echo "<legend>Insira os dados abaixo:</legend>";
+        echo "<fieldset>";
+        echo "<div>";
+        echo "Nome: <input type='text' class='form-control' name='nome' value='" . $cliente['nome'] . "'>";
+        echo "</div>";
+        echo "<div>";
+        echo "CPF: <input type='text' class='form-control' name='cpf' value='" . $cliente['cpf'] . "'>";
+        echo "</div>";
+        echo "<div>";
+        echo "Email: <input type='email' class='form-control' name='email' value='" . $cliente['email'] . "'>";
+        echo "</div>";
+        echo "<div>";
+        echo "<input type='submit' class='btn btn-primary' value='Enviar'>";
+        echo "<input type='reset' class='btn btn-primary' value='Limpar Dados'>";
+        echo "</div>";
+        echo "<br>";
+        echo "</fieldset>";
+        echo "</form>";
+    }
+}
+
+if (isset($_POST['nome'])) {
+    $sql = $pdo->prepare("UPDATE tab_clientes SET nome = ?, cpf = ?, email = ? WHERE cod_cliente = $cod_cliente");
+    $sql->execute(array($_POST['nome'], $_POST['cpf'], $_POST['email']));
+    echo "<h1>Usuário com id = $cod_cliente alterado com sucesso!</h1>";
+    echo "<button class='btn btn-primary'><a href='index.php'>Voltar</a></button>";
+
+    //echo "<h1>Alterado com sucesso!</h1>";
+}
---
+++ index.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CRUD em PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+  <body>
+
+  <?php
+    $pdo = new PDO("mysql:host=localhost;dbname=cadastro_cliente", "root", "");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    if (isset($_GET['excluir'])){
+        $cod_cliente = (int) $_GET['excluir'];
+        $pdo->exec("DELETE FROM tab_clientes WHERE cod_cliente = $cod_cliente");
+        echo "<h3>Cliente $cod_cliente excluído com sucesso.</h3>";
+        header("Location: index.php");
+    };
+
+    if(isset($_POST['nome'])){
+        $sql = $pdo->prepare("INSERT INTO `tab_clientes` VALUES (null, ?,?,?)");
+        $nome = $_POST['nome'];
+        $sql->execute(array($_POST['nome'], $_POST['cpf'], $_POST['email']));
+        echo "<h3>Cliente $nome cadastrado com sucesso.</h3>";
+    }
+  ?>
+    <div class="container">
+        <form method="POST">
+        <legend><h2 class="row justify-content-center">Cadastro de clientes</h2></legend>
+        <fieldset>
+            <div>
+                Nome: <input type="text" name="nome" class="form-control">
+            </div>
+            <div>
+                CPF: <input type="text" name="cpf" class="form-control">
+            </div>
+            <div>
+                Email: <input type="email" name="email" class="form-control">
+            </div>
+            <div>
+                <button type="submit" class="btn btn-primary" value="Enviar">Enviar</button>
+                <button type="reset" class="btn btn-primary" value="Limpar dados">Limpar dados</button>
+            </div>
+        </form>
+        </fieldset>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+    <?php 
+        $sql = $pdo->prepare("SELECT * FROM `tab_clientes`");
+        $sql-> execute();
+        $clientes = $sql->fetchAll();
+        
+        echo "<table class= 'table table-stripped table-hover'>";
+        echo "<thead>";
+        echo "<tr>";
+        echo "<th scope='col' colspan='2' class='text-center'>Ações</th>";
+        echo "<th scope='col'>Nome</th>";
+        echo "<th scope='col'>CPF</th>";
+        echo "<th scope='col'>Email</th>";
+        echo "</tr></thead><tbody>";
+
+        foreach($clientes as $cliente){
+            echo "<tr>";
+            echo '<td align=center><a href="?excluir='. $cliente['cod_cliente'] . '">( X )</a></td>';
+            echo '<td align=center><a href="alterar.php?cod_cliente='. $cliente['cod_cliente'] . '">( Alterar )</a></td>';
+            echo "<td>" . $cliente['nome'] . "</td>";
+            echo "<td>" . $cliente['cpf'] . "</td>";
+            echo "<td>" . $cliente['email'] . "</td>";
+            echo "</tr>";
+        }
+    ?>
+
+  </body>
+</html>
